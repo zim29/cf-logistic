75c90e0663ff2f4d050ae1c1b66e26613094c8e4
Jose Rosa <jrosamanan@gmail.com>
2024-04-10 07:52:10 -0400

    commit

--- admin.cftransportes.com/database/seeders/DatabaseSeeder.php
+++ admin.cftransportes.com/database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ use App\Models\Client;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -27,6 +28,16 @@ class DatabaseSeeder extends Seeder
         
         User::factory(10)->create();
         Client::factory(50)->create();
+
+
+        $users = [
+            ['name' => 'Admin', 'email'=> 'admin@example.com', 'password'=> Hash::make('password'), 'role_id' => 1],
+            ['name' => 'finanzas', 'email'=> 'finanzas@example.com', 'password'=> Hash::make('password'), 'role_id' => 2],
+            ['name' => 'ventas', 'email'=> 'ventas@example.com', 'password'=> Hash::make('password'), 'role_id' => 3],
+            ['name' => 'Operaciones', 'email'=> 'admin@example.com', 'password'=> Hash::make('password'), 'role_id' => 4],
+            ['name' => 'Delivery', 'email'=> 'admin@example.com', 'password'=> Hash::make('password'), 'role_id' => 5],
+            ['name' => 'Cliente', 'email'=> 'admin@example.com', 'password'=> Hash::make('password'), 'role_id' => 6, 'client_id' => 1],
+        ];
 
     }
 }
